b329e2bd2b2df2d0b23e43fb79c71d8e305c4c06
Ömer Tunç <omertuncwho@gmail.com>
2024-01-27 23:12:56 +0300

    Degree to Radian/Grad

---
+++ Degree to Radian/main.js
@@ -0,0 +1,9 @@
+function degreeToRadian (d){
+    pi=Math.PI;
+let radian = d*pi/180;
+console.log(`Radian result: ${radian}`);
+let grad= d*200/180;
+console.log(`Grad result: ${grad}`);
+}
+
+degreeToRadian(100);
