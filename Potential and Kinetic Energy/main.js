b0b48359039f826313839d44803a17860aefa1ff
Ömer Tunç <omertuncwho@gmail.com>
2024-01-28 22:51:35 +0300

    Potential and Kinetic Energy

---
+++ Potential and Kinetic Energy/main.js
@@ -0,0 +1,12 @@
+function potential(m,h){
+let calculate = m*9.8*h;
+console.log(`Potential energy is: ${calculate}`);
+}
+potential(3,12);
+
+function kinetic(m,v){
+    calculate = m*v**2/2;
+    console.log(`Kinetic energy is: ${calculate}`)
+
+}
+kinetic(3,44)
