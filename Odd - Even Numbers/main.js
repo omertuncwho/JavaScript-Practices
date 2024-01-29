685c7783e1b2b23828f6af6bb91987b962526084
Ömer Tunç <omertuncwho@gmail.com>
2024-01-29 09:31:38 +0300

    Odd - Even Numbers

---
+++ Odd - Even Numbers/main.js
@@ -0,0 +1,10 @@
+function oddEven(n) {
+    if (n % 2 === 0) {
+      console.log(`The entered integer is even.`);
+    } else {
+      console.log(`The entered integer is odd.`);
+    }
+  }
+  
+  oddEven(12);
+  
